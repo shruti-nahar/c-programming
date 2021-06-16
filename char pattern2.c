e74a682e3197fcc090518a81deeb2be734493fcf
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-06-16 11:01:51 +0530

    character printing

---
+++ char pattern2.c
@@ -0,0 +1,26 @@
+/////////////////////////////////////
+/*Input:--
+*Output:
+    A
+    B    B
+    C    C    C
+    D    D    D    D
+    E    E    E    E    E
+*Description:Character Pattern
+*Date:16-June-2021
+*Author:Shruti Nahar*/ 
+/////////////////////////////////////////
+#include<stdio.h>
+int main()
+{
+int i,j,ch=65;
+for(i=1;i<=5;i++)
+{
+for(j=1;j<=i;j++)
+{
+printf("%5c",ch);
+}
+ch++;
+printf("\n");
+}
+}
