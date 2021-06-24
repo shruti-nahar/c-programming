05fed34360a5983de27e627a40c2239479ff5d5e
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-06-24 09:15:42 +0530

    Use of logical and operator in for loop

---
+++ logical.c
@@ -0,0 +1,19 @@
+//////////////////////////////////////
+/*Input:--
+*Output: 0
+         1
+         1
+*Description: use of logical and in for loop
+*Date: 24-June-2021
+*Author: Shruti Nahar */
+/////////////////////////////////////////
+#include<stdio.h>
+int main()
+{
+for(int i=0;i<5;i++)
+{
+printf("%d",i&&1);
+printf("\n");
+i++;
+}
+}
