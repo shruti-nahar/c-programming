1697244bf28db17b486f58ff008463628ad13100
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-06-25 10:33:58 +0530

    Display Hello World!!!

--- basic.c
+++ basic.c
@@ -1,3 +1,10 @@
+//////////////////////////////////////
+/*Input: --
+*Output: Hello world !!!
+*Description:Display Hello World 
+*Date: 25-June-2021
+*Author: Shruti Nahar */
+/////////////////////////////////////////
 #include<stdio.h>
 int main()
 {
