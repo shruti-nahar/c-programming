ba95346cb700de844ad90a2e04e6676d5a0cf07f
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-07-01 21:41:33 +0530

    Basic use of while loop

---
+++ while basic.c
@@ -0,0 +1,29 @@
+////////////////////////
+/*Input:--
+*Output:10
+ 9
+ 8
+ 7
+ 6
+ 5
+ 4
+ 3
+ 2
+ 1
+thankyou
+*Description:Basic use of while loop
+*Date:1-July-2021
+*Author name: Shruti Nahar*/
+#include<stdio.h>
+int main()
+{
+int i;
+i=10;
+while(i>=1)
+{
+printf("\n %d",i);
+i--;
+}
+printf("\nthankyou");
+
+}
