cf160ae1fdb318e0380ddb05deaff8bd833b8859
Shruti Rajendra Nahar <84951018+shruti-nahar@users.noreply.github.com>
2021-07-23 09:05:57 +0530

    For loop implementation

---
+++ demo.c
@@ -0,0 +1,17 @@
+/////////////////////////////////////
+/*Input: --
+*Output:33
+*Description:For loop implementation
+*Date: 23-July-2021
+*Author: Shruti Nahar*/ 
+/////////////////////////////////////////
+#include<stdio.h>
+int main()
+{
+int i=3,j=3;
+for(;i<6,j<4;i++,j++)
+{
+printf("%d",i);
+printf("%d",j);
+}
+}
