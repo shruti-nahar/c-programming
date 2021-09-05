bd4ae93a34566f73747543872a9a24a62ad8f991
Shruti Rajendra Nahar <84951018+shruti-nahar14@users.noreply.github.com>
2021-09-05 22:37:32 +0530

    print the pattern

---
+++ LB102.c
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////
+//Function Name:Pattern
+//Input:5
+//Output:5       #       4       #       3       #       2       #       1       #
+//Description:Print the Pattern
+//Date: 05/09/2021
+//Author: Shruti Nahar
+//////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void Pattern(int iNo)
+{
+	int i=0;
+
+	for(i=iNo;i>=1;i--)
+	{
+		printf("%d\t#\t",i);
+	}
+
+}
+
+int main()
+{
+	int iNo=0,iRet=0;
+
+	printf("Enter a Number");
+	scanf("%d",&iNo);
+
+	Pattern(iNo);
+
+	return 0;
+}
